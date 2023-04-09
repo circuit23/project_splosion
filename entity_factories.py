666216791f4869b22906fb93c0288780fbbcdcf4
circuit23 <circuit.23@gmail.com>
2023-04-09 14:24:09 -0400

    add equipment items (weapons & armor) add a system to store their stats, add them to the player's object, track equipped items

--- entity_factories.py
+++ entity_factories.py
@@ -1,5 +1,6 @@
 from components.ai import HostileEnemy
-from components import consumable
+from components import consumable, equippable
+from components.equipment import Equipment
 from components.fighter import Fighter
 from components.inventory import Inventory
 from components.level import Level
@@ -10,6 +11,7 @@ player = Actor(
     color=(255, 255, 255),
     name="Player",
     ai_cls=HostileEnemy,
+    equipment=Equipment(),
     fighter=Fighter(hp=30, defense=2, power=5),
     inventory=Inventory(capacity=26),
     level=Level(level_up_base=200),
@@ -20,6 +22,7 @@ orc = Actor(
     color=(63, 127, 63),
     name="Orc",
     ai_cls=HostileEnemy,
+    equipment=Equipment(),
     fighter=Fighter(hp=10, defense=0, power=3),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=35),
@@ -29,6 +32,7 @@ troll = Actor(
     color=(0, 127, 0),
     name="Troll",
     ai_cls=HostileEnemy,
+    equipment=Equipment(),
     fighter=Fighter(hp=16, defense=1, power=4),
     inventory=Inventory(capacity=0),
     level=Level(xp_given=100),
@@ -57,4 +61,21 @@ lightning_scroll = Item(
     color=(255, 255, 255),
     name="Lightning Scroll",
     consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
+)
+
+dagger = Item(
+    char="/", color=(0, 191, 255), name="Dagger", equippable=equippable.Dagger()
+)
+
+sword = Item(char="/", color=(0, 191, 255), name="Sword", equippable=equippable.Sword())
+
+leather_armor = Item(
+    char="[",
+    color=(139, 69, 99),
+    name="Leather Armor",
+    equippable=equippable.LeatherArmor(),
+)
+
+chain_mail = Item(
+    char="[", color=(139, 69, 99), name="Chain Mail", equippable=equippable.ChainMail()
 )
