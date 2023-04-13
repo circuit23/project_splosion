71ea681b70b5c540c7f62a8011af8dfeff05fe8b
circuit23 <circuit.23@gmail.com>
2023-04-13 09:44:58 -0400

    switch over to cp437 tileset from REXPaint for consistency

--- components/tileset.py
+++ components/tileset.py
@@ -7,18 +7,19 @@ def tile_set():
     :return:
     """
     tileset = tcod.tileset.load_tilesheet(
-        "TiledFont.png", 32, 10, tcod.tileset.CHARMAP_TCOD
+        "cp437_20x20.png", 16, 16, tcod.tileset.CHARMAP_CP437
     )
-    tileset.remap(0x100000, 2, 5)  # Assign codepoint 0x100000 to a character sprite
-    tileset.remap(0x100001, 3, 5)  # Orcs
-    tileset.remap(0x100002, 4, 5)  # Trolls
-    tileset.remap(0x100003, 5, 5)  # Scrolls
-    tileset.remap(0x100004, 6, 5)  # Potions
-    tileset.remap(0x100005, 7, 5)  # sword
-    tileset.remap(0x100008, 10, 5)  # dagger
-    tileset.remap(0x100006, 8, 5)  # armor
-    tileset.remap(0x100007, 9, 5)  # downstairs
-    tileset.remap(0x100009, 0, 5)  # wall
-    tileset.remap(0x100010, 1, 5)  # floor
+    # follow the format of these old codepoint assignments to use new ones
+    # tileset.remap(0x100000, 2, 5)  # Assign codepoint 0x100000 to a character sprite
+    # tileset.remap(0x100001, 3, 5)  # Orcs
+    # tileset.remap(0x100002, 4, 5)  # Trolls
+    # tileset.remap(0x100003, 5, 5)  # Scrolls
+    # tileset.remap(0x100004, 6, 5)  # Potions
+    # tileset.remap(0x100005, 7, 5)  # sword
+    # tileset.remap(0x100008, 10, 5)  # dagger
+    # tileset.remap(0x100006, 8, 5)  # armor
+    # tileset.remap(0x100007, 9, 5)  # downstairs
+    # tileset.remap(0x100009, 0, 5)  # wall
+    # tileset.remap(0x100010, 1, 5)  # floor
 
     return tileset
